231d9ff4739dcd2795daa50a0a209acec0560a23
Thuận Đình <66295546+ThuanNguyen386@users.noreply.github.com>
2023-03-30 10:05:00 +0700

    Add files via upload

---
+++ QuanLyNhanSu/src/main/java/app/QuanLyNhanSu/QuanLyNhanSuApplication.java
@@ -0,0 +1,12 @@
+package app.QuanLyNhanSu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyNhanSuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuanLyNhanSuApplication.class, args);
+    }
+}
---
+++ QuanLyNhanSu/src/main/java/app/QuanLyNhanSu/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package app.QuanLyNhanSu.repository;
+
+import app.QuanLyNhanSu.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {}
