808ec8dc5973474ed3b263042ba5fb516ea4abd8
Thuận Đình <66295546+ThuanNguyen386@users.noreply.github.com>
2023-03-30 10:06:58 +0700

    Delete QuanLyNhanSu/src/main/java/DuAn1/QuanLyNhanSu directory

--- QuanLyNhanSu/src/main/java/DuAn1/QuanLyNhanSu/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package DuAn1.QuanLyNhanSu.repository;
-
-import DuAn1.QuanLyNhanSu.model.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, Long> {}
